c3b9262d0765bfaee6e83463b6d887843cbd0385
Baptiste <baptiste.stablo1@gmail.com>
2023-09-03 17:19:54 +0200

    quotes

--- ft_check_quotes.c
+++ ft_check_quotes.c
@@ -1,14 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   whitespace_skip.c                                  :+:      :+:    :+:   */
+/*   ft_check_quotes.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bstablo <bstablo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/03 13:47:16 by bstablo           #+#    #+#             */
-/*   Updated: 2023/09/03 13:54:47 by bstablo          ###   ########.fr       */
+/*   Created: 2023/09/03 17:09:56 by bstablo           #+#    #+#             */
+/*   Updated: 2023/09/03 17:18:35 by bstablo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	ft_check_quotes(char *str)
+{
+	int	n;
+	int	i;
+	int	j;
+
+	n = 0;
+	i = 0;
+	j = 0;
+	while (str[n])
+	{
+		if (str[n] == 34)
+			i++;
+		else if (str[n] == 39)
+			j++;
+		n++;
+	}
+	if (i % 2 != 0 || j % 2 != 0)
+		return (-1);
+	return (0);
+}
